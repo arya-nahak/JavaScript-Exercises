0705638a41dd42fef3f1376dc1f6791c50af4588
kanu.nahak.79@gmail.com <kanu@mz.local>
2024-04-04 12:40:43 +0530

    Added new program

--- Prime Number/index.js
+++ Prime Number/index.js
@@ -1,5 +1,22 @@
-const Prime_number_checker = (num)=>{
-    if(num / 1 && num)
-}
+const Prime_number_checker = (num) => {
+  if (num < 2) {
+    return false;
+  }
 
-let result = Prime_number_checker(4)
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+let num = 6;
+let result = Prime_number_checker(num);
+
+if (result) {
+  console.log(num + " is a prime number");
+} else {
+  console.log(num + " is not a prime number");
+}
